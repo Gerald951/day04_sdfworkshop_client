c5bcf9bf0bd6114ad435f1da8ca67eba297cb70b
Gerald Ng Qi Long <ngqilong@gmail.com>
2023-01-17 15:55:25 +0800

    Updated with IO Stream

--- App.java
+++ App.java
@@ -1,5 +1,16 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 /**
  * Hello world!
  */
@@ -11,7 +22,47 @@ public final class App {
      * Says hello to the world.
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("local host", 7000);
+
+        try (OutputStream os = socket.getOutputStream()) {
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            DataOutputStream dos = new DataOutputStream(bos);
+
+            Console con = System.console();
+            String readInput = "";
+
+            while (!readInput.equalsIgnoreCase("close")) {
+                readInput = con.readLine("Enter a command: ");
+                dos.writeUTF(readInput);
+                dos.flush();
+
+            }
+
+        } catch (EOFException ex) {
+            socket.close();
+        }
+
+        Socket socket = new Socket("local host", 7000);
+
+        try (InputStream is = socket.getInputStream()) {
+            BufferedInputStream bis = new BufferedInputStream(is);
+            DataInputStream dis = new DataInputStream(bis);
+
+            while (!readInput.equalsIgnoreCase("close")) {
+                readInput = is.readLine("Enter a command: ");
+                dis.writeUTF(readInput);
+                dis.flush();
+
+                String receivedMessaged = dis.readUTF();
+                System.out.println(receivedMessaged);
+
+            }
+            dis.close();
+            bis.close();
+            is.close();
+        } catch (EOFException ex) {
+            socket.close();
+        }
     }
 }
